1792915a53f20040d7d09ab061c0a3e116907653
Michael Bayne <mdb@samskivert.com>
2014-04-25 14:55:44 -0700

    Indent fixes, added electric-newline.
    We need to do our case detection last(ish) because it is basically a special
    kind of block and we want other indent rules to have a chance to match first.
    
    Fn electric-newline does "smart stuff" when you press ENTER. Currently that
    includes one smart thing which is adding "* " and indenting properly when you
    press newline in the middle of a scaladoc comment.

--- src/main/scala/scaled/scala/ScalaMode.scala
+++ src/main/scala/scaled/scala/ScalaMode.scala
@@ -7,7 +7,7 @@ package scaled.scala
 import scaled._
 import scaled.grammar.{Grammar, GrammarCodeConfig, GrammarCodeMode}
 import scaled.major.CodeConfig
-import scaled.util.Indenter
+import scaled.util.{Chars, Indenter}
 
 object ScalaConfig extends Config.Defs {
   import EditorConfig._
@@ -48,8 +48,16 @@ object ScalaConfig extends Config.Defs {
        ints=Array("scala"),
        desc="A major editing mode for the Scala language.")
 class ScalaMode (env :Env) extends GrammarCodeMode(env) {
+  import CodeConfig._
+  import Chars._
 
   override def configDefs = ScalaConfig :: super.configDefs
+
+  override def keymap = super.keymap ++ Seq(
+    "ENTER"   -> "electric-newline",
+    "S-ENTER" -> "electric-newline"
+  )
+
   override protected def grammars = ScalaConfig.grammars
   override protected def effacers = ScalaConfig.effacers
 
@@ -67,14 +75,35 @@ class ScalaMode (env :Env) extends GrammarCodeMode(env) {
     new Indenter.IfElseIfElseAlign(config, buffer),
     new ScalaIndenter.ValueExprBody(config, buffer),
     new ScalaIndenter.Extends(config, buffer),
-    new ScalaIndenter.CaseBody(config, buffer),
     new ScalaIndenter.Scaladoc(config, buffer),
-    new Indenter.OneLinerWithArgs(config, buffer, Set("if", "while", "for")),
+    new Indenter.OneLinerWithArgs(config, buffer, blocker, Set("if", "while", "for")),
     new Indenter.OneLinerNoArgs(config, buffer, Set("else", "do", "try", "finally")),
+    new ScalaIndenter.CaseBody(config, buffer),
     new Indenter.ByBlock(config, buffer) {
       override def readBlockIndent (pos :Loc) = ScalaIndenter.readBlockIndent(buffer, pos)
     }
   ) ++ super.createIndenters()
+
+  //
+  // FNs
+
+  @Fn("""Inserts a newline, then indents the subsequent line. Handles other "smart" cases such as:
+         If newline is inserted in the middle of a Scaladoc comment, the next line is prepended with
+         * before indenting. TODO: other smarts.""")
+  def electricNewline () {
+    val p = view.point()
+    val onDoc = buffer.stylesAt(p) contains docStyle
+    val afterDoc = (p.col > 0) && (buffer.stylesAt(p.prevC) contains docStyle)
+    val afterCloseDoc = buffer.line(p).lastIndexOf(closeDocM, p.col) != -1
+    val inDoc = onDoc || (afterDoc && !afterCloseDoc)
+    newline()
+    if (inDoc) {
+      buffer.insert(view.point(), "* ", Styles.None)
+      view.point() = view.point() + (0, 2)
+    }
+    reindentAtPoint()
+  }
+  private val closeDocM = Matcher.exact("*/")
 
   // TODO: more things!
 }
