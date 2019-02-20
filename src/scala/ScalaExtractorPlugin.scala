85b9eb46b7caac059eafbf7b9fe29605a5a9016e
Michael Bayne <mdb@samskivert.com>
2019-02-20 13:14:42 -0800

    Check depends classpath, not "logical" depends.
    Some projects may not be able to expose logical depends but still have a valid
    compilation classpath (like via .dotty-ide.json or Eclipse .classpath files).

--- src/scala/ScalaExtractorPlugin.scala
+++ src/scala/ScalaExtractorPlugin.scala
@@ -17,7 +17,7 @@ class ScalaExtractorPlugin extends ExtractorPlugin {
   // only return an extractor if our project has valid dependencies (it'll at least need a
   // scala-library dependency otherwise we're going to be hosed)
   override def extractor (project :Project, suff :String) =
-    if (project.depends.ids.isEmpty) None else Some(new ScalaExtractor() {
+    if (exClasspath(project).isEmpty) None else Some(new ScalaExtractor() {
       override def compilerArgs = exScalacOpts(project)
       override def classpath = exClasspath(project)
       override def log (msg :String) = project.log(msg)
@@ -26,15 +26,15 @@ class ScalaExtractorPlugin extends ExtractorPlugin {
   private def exScalacOpts (project :Project) = scala.List() ++ (
     project.component(classOf[ScalaCompiler]) map { _.scalacOpts } getOrElse Seq()).toSeq.toScala
 
-  private def exClasspath (project :Project) = (project.component(classOf[JavaComponent]) map {
-    jp =>
-    // if this project doesn't have scala-library on its classpath, we have to put it there; the
-    // only project that's likely not to have scala-library on its classpath is scala-library
-    // itself, but that's one we definitely want to index
-    val cp = jp.buildClasspath
-    if (cp.exists(_.getFileName.toString.contains("scala-library"))) cp
-    else (cp ++ findScalaLibrary)
-  } getOrElse Seq()).toSeq.toScala
+  private def exClasspath (project :Project) =
+    (project.component(classOf[JavaComponent]) map { jp =>
+      // if this project doesn't have scala-library on its classpath, we have to put it there; the
+      // only project that's likely not to have scala-library on its classpath is scala-library
+      // itself, but that's one we definitely want to index
+      val cp = jp.buildClasspath
+      if (cp.exists(_.getFileName.toString.contains("scala-library"))) cp
+      else (cp ++ findScalaLibrary)
+    } getOrElse Seq()).toSeq.toScala
 
   private def findScalaLibrary :Option[Path] = {
     val loader = classOf[scala.Option[_]].getClassLoader.asInstanceOf[URLClassLoader]
